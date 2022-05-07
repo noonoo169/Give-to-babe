8753b98a292c1b6eacfc8aff0eb7aa8f7c0c9202
noonoo169 <88936808+noonoo169@users.noreply.github.com>
2022-05-07 09:57:13 +0700

    Add files via upload
    1

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Ngọc Huế à!',
+    introDesc: `Anh có món quà tinh thần muốn tặng em để em vui vẻ hơn trong những ngày đèn đỏ bực rọc <3`,
+    btnIntro: 'Em cảm ơn',
+    title: 'Em iu của anh có yêu anh hông nè',
+    btnYes: 'Luôn luôn yêu anh <33',
+    btnNo: 'Không, đồng chí à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại được em chọn làm ny <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh là người mà em yêu và là định mệnh của em <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Bấm nút dưới này là sẽ gặp anh đó.',
+    btnAccept: 'Yêu anh',
+    messLink: 'https://m.me/100008822856437' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
